a37c05d8b699d54d69418703f90a252cfea47127
ishraqe <kanak431@diu.edu.bd>
2018-11-07 20:23:31 +0600

    news section

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -23,6 +23,127 @@ class Home extends Component {
             </div>
           </div>
         </section>
+        <section id="news">
+          <div className="container">
+            <div className="wrapper">
+              <div className="container-fixed">
+                <div className="news-container">
+                  <div className="items equal-width ">
+                    <div className="item horizontal-item">
+                      <div className="img-container">
+                        <img
+                          className="news-image"
+                          src={require("../assets/img/news-1.jpeg")}
+                          alt=""
+                        />
+                      </div>
+
+                      <div className="info-container">
+                        <h2 className="sub-title">park life</h2>
+                        <h1 className="main-title">N 1</h1>
+                        <p className="info-para">
+                          Lorem ipsum dolor sit amet consectetur adipisicing
+                          elit. Est illo, perferendis sequi numquam ad ut
+                          ratione possimus voluptates deleniti eaque quo,
+                          laboriosam quos itaque tempora delectus error
+                          assumenda et. Perspiciatis?
+                        </p>
+                      </div>
+                    </div>
+                    <div className="item horizontal-item">
+                      <div className="img-container">
+                        <img
+                          className="news-image"
+                          src={require("../assets/img/news-2.jpg")}
+                          alt=""
+                        />
+                      </div>
+
+                      <div className="info-container">
+                        <h2 className="sub-title">park life</h2>
+                        <h1 className="main-title">N 1</h1>
+                        <p className="info-para">
+                          Lorem ipsum dolor sit amet consectetur adipisicing
+                          elit. Est illo, perferendis sequi numquam ad ut
+                          ratione possimus voluptates deleniti eaque quo,
+                          laboriosam quos itaque tempora delectus error
+                          assumenda et. Perspiciatis?
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="items small-container">
+                    <div className="item full-item">
+                      <div className="img-container">
+                        <img
+                          className="news-image"
+                          src={require("../assets/img/news-2.jpg")}
+                          alt=""
+                        />
+                      </div>
+
+                      <div className="info-container">
+                        <h2 className="sub-title">park life</h2>
+                        <h1 className="main-title">N 1</h1>
+                        <p className="info-para">
+                          Lorem ipsum dolor sit amet consectetur adipisicing
+                          elit. Est illo, perferendis sequi numquam ad ut
+                          ratione possimus voluptates deleniti eaque quo,
+                          laboriosam quos itaque tempora delectus error
+                          assumenda et. Perspiciatis?
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="items equal-width ">
+                    <div className="item vertical-item">
+                      <div className="img-container">
+                        <img
+                          className="news-image"
+                          src={require("../assets/img/news-1.jpeg")}
+                          alt=""
+                        />
+                      </div>
+
+                      <div className="info-container">
+                        <h2 className="sub-title">park life</h2>
+                        <h1 className="main-title">N 1</h1>
+                        <p className="info-para">
+                          Lorem ipsum dolor sit amet consectetur adipisicing
+                          elit. Est illo, perferendis sequi numquam ad ut
+                          ratione possimus voluptates deleniti eaque quo,
+                          laboriosam quos itaque tempora delectus error
+                          assumenda et. Perspiciatis?
+                        </p>
+                      </div>
+                    </div>
+                    <div className="item horizontal-item">
+                      <div className="img-container">
+                        <img
+                          className="news-image"
+                          src={require("../assets/img/news-2.jpg")}
+                          alt=""
+                        />
+                      </div>
+
+                      <div className="info-container">
+                        <h2 className="sub-title">park life</h2>
+                        <h1 className="main-title">N 1</h1>
+                        <p className="info-para">
+                          Lorem ipsum dolor sit amet consectetur adipisicing
+                          elit. Est illo, perferendis sequi numquam ad ut
+                          ratione possimus voluptates deleniti eaque quo,
+                          laboriosam quos itaque tempora delectus error
+                          assumenda et. Perspiciatis?
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
       </>
     );
   }
